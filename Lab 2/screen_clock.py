81933eea03bce4d6782feee8b451378d8f290da3
Demi Hu <demihu1116@gmail.com>
2026-09-20 12:54:26 -0400

    implemented animation

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -1,5 +1,6 @@
 import time
 import subprocess
+from pathlib import Path
 import digitalio
 import board
 from PIL import Image, ImageDraw, ImageFont
@@ -60,12 +61,26 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+
+def animation(filename, number_of_frames, time_between_frames):
+    for frame_number in range(1, number_of_frames + 1):
+        frame_path = Path(__file__).parent / "animation" / f"{filename}{frame_number}.png"
+        with Image.open(frame_path) as source:
+            frame = source.convert("RGB").resize((width, height))
+        disp.image(frame, rotation)
+        time.sleep(time_between_frames)
+
+
 while True:
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=400)
+    current_hour = int(time.strftime("%H"))
+    if 6 <= current_hour < 12:
+        animation("m", 4, 0.25)
+    elif 12 <= current_hour < 17:
+        animation("a", 2, 0.5)
+    elif 17 <= current_hour < 21:
+        animation("e", 4, 0.25)
+    else:
+        animation("n", 2, 0.5)
 
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+        
 
-    # Display image.
-    disp.image(image, rotation)
-    time.sleep(1)
